476ae9ad6288f3baa57e273a3869f31f392107c3
VashCosta <vivashvel2019@gmail.com>
2026-04-12 13:11:48 +0530

    Initial commit: VREZER AI Spring Boot platform

---
+++ src/main/java/com/resume/analyzer/service/PdfExtractorService.java
@@ -0,0 +1,19 @@
+package com.resume.analyzer.service;
+
+import org.apache.pdfbox.Loader;
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.text.PDFTextStripper;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+import java.io.IOException;
+
+@Service
+public class PdfExtractorService {
+
+    public String extractTextFromPdf(MultipartFile file) throws IOException {
+        try (PDDocument document = Loader.loadPDF(file.getBytes())) {
+            PDFTextStripper stripper = new PDFTextStripper();
+            return stripper.getText(document);
+        }
+    }
+}
